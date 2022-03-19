1d0351a9e23e90a5a3d5126392f0e8f1c42abf5d
EMPTY01 <sergey.chuyckow2016@yandex.ru>
2022-03-20 00:09:22 +0700

    19.03

--- flappy_bird/draw.js
+++ flappy_bird/draw.js
@@ -7,7 +7,7 @@ let fg = new Image(); // Создание объекта
 let pipeUp = new Image(); // Создание объекта
 let pipeBottom = new Image(); // Создание объекта
 
-bird.src = "img/bird.jpeg"; // Указание нужного изображения
+bird.src = "img/bird.png"; // Указание нужного изображения
 bg.src = "img/bg.png"; // Аналогично
 fg.src = "img/fg.png"; // Аналогично
 pipeUp.src = "img/pipeUp.png"; // Аналогично
